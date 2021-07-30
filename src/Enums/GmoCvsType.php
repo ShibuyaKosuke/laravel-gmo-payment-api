9ab6c47cf600663fc2de8d56e549d57e629b75f1
ShibuyaKosuke <shibuya@imagenious.co.jp>
2021-07-30 14:45:00 +0900

    mod

--- src/Enums/GmoCvsType.php
+++ src/Enums/GmoCvsType.php
@@ -15,4 +15,26 @@ final class GmoCvsType extends Enum
     public const MINI_STOP = '10005';
     public const SEVEN_ELEVEN = '00007';
     public const SEIKO_MART = '10008';
+
+    /**
+     * @param mixed $value
+     * @return string
+     */
+    public static function getDescription($value): string
+    {
+        switch ($value) {
+            case self::LAWSON:
+                return 'ローソン';
+            case self::FAMILY_MART:
+                return 'ファミリーマート';
+            case self::MINI_STOP:
+                return 'ミニストップ';
+            case self::SEVEN_ELEVEN:
+                return 'セブンイレブン';
+            case self::SEIKO_MART:
+                return 'セイコーマート';
+            default:
+                return $value;
+        }
+    }
 }
